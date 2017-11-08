6093c6d3e27f7c0e887d94778a760dfec3aeb293
Gerjan Metz <metz1503@student.nhl.nl>
2017-11-08 10:51:50 +0100

    add calcDistance util

--- js/utils.js
+++ js/utils.js
@@ -2,4 +2,13 @@ class Utils {
     constructor() {
         console.log("utils constructor");
     }
+
+    calcDistance(object1, object2) {
+        let dx = object2.position.x - object1.position.x;
+        let dy = object2.position.z - object1.position.z;
+        let dxSquared = dx * dx;
+        let dySquared = dy * dy;
+        let sum = dxSquared + dySquared;
+        return Math.sqrt(sum);
+    }
 }
