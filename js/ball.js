70d4c3a35c7f2d52ee8ca4991b1a2a482f58020e
Gerjan Metz <metz1503@student.nhl.nl>
2017-11-08 10:53:13 +0100

    add ball class

---
+++ js/ball.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Gerjan on 8-10-2017.
+ */
+class Ball extends THREE.Mesh{
+    constructor(){
+        let ballGeometry = new THREE.SphereGeometry(ballRadius);
+        let ballMaterial = new THREE.MeshPhongMaterial({ });
+        super(ballGeometry, ballMaterial);
+
+        this.xSpeed = 0.0;
+        this.zSpeed = 0.0;
+        this.mass = 1;
+        this.Colliding = false;
+
+        this.oldPosX = 0;
+        this.oldPosZ = 0;
+
+        this.receiveShadow = true;
+        this.castShadow = true;
+
+        scene.add(this);
+    }
+
+    get GetXSpeed() {
+        return this.xSpeed;
+    }
+
+    get GetZSpeed() {
+        return this.zSpeed;
+    }
+
+    get GetVelocity() {
+        return Math.sqrt(this.xSpeed * this.xSpeed +  this.zSpeed * this.zSpeed);
+    }
+
+    get GetDirection() {
+        return Math.atan2(this.zSpeed, this.xSpeed);
+    }
+
+    get GetMass() {
+        return this.mass;
+    }
+
+    get isColliding() {
+        return this.Colliding;
+    }
+
+    get GetOldX() {
+        return this.oldPosX;
+    }
+
+    get GetOldZ() {
+        return this.oldPosZ;
+    }
+
+    set SetXSpeed(value){
+        this.xSpeed = value;
+    }
+
+    set SetZSpeed(value){
+        this.zSpeed = value;
+    }
+
+    set isColliding(value) {
+        this.Colliding = value;
+    }
+
+    set SetOldX(value) {
+        this.oldPosX = value;
+    }
+
+    set SetOldZ(value) {
+        this.oldPosZ = value;
+    }
+}
--- js/utils.js
+++ js/utils.js
@@ -3,6 +3,7 @@ class Utils {
         console.log("utils constructor");
     }
 
+    // calculate distance between objects
     calcDistance(object1, object2) {
         let dx = object2.position.x - object1.position.x;
         let dy = object2.position.z - object1.position.z;
