9d8bcc6da9d7967b26727ee6055dbf504eb1794f
Gerjan Metz <metz1503@student.nhl.nl>
2017-11-08 14:40:44 +0100

    mod orbitcontrols, add click event

--- js/events.js
+++ js/events.js
@@ -2,10 +2,18 @@ class Events {
     constructor() {
         console.log("events constructor");
 
+        gameCore.controls.mouseButtons.ORBIT = THREE.MOUSE.RIGHT;
+        gameCore.controls.mouseButtons.PAN = null;
+
         window.addEventListener('resize', () => {
             gameCore.camera.aspect = window.innerWidth / window.innerHeight;
             gameCore.camera.updateProjectionMatrix();
             gameCore.renderer.setSize(window.innerWidth, window.innerHeight);
+        });
+
+        window.addEventListener('click', () => {
+            console.log("onclick");
+            gameCore.physics.shootBall();
         });
     }
 }
